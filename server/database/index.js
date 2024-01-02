20497c9cb9aff1e3a7ab3f0fb9a33f69281b04ce
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2024-01-02 14:19:54 +0300

    feat: configure a database connection

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = new Sequelize('Web_platforma_dlya_khudozhestvennykh_auktsionov', 'postgres', 'admin', {
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
