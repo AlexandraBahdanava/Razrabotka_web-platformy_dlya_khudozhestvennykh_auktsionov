745e5a1c81c8fb61397984f08a8a7d14037f9ffe
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2024-01-03 13:49:37 +0300

    feat: add routes template

---
+++ client/src/router/artistRoutes.js
@@ -0,0 +1,22 @@
+export const artistRoutes = [
+    {
+        path: "/artist",
+        Component: "My orders page",
+    },
+    {
+        path: "/artist/:id",
+        Component: "Order details page",
+    },
+    {
+        path: "/artist/:id",
+        Component: "User details page",
+    },
+    {
+        path: "/orders/create",
+        Component: "Order Creation Page",
+    },
+    {
+        path: "/profile",
+        Component: "Company profile",
+    },
+];
