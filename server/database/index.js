011249f7dc1c29e2e7c377998cc33d38ce1fbf2b
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2024-01-10 01:08:30 +0300

    fix: remove conflicting files for merge

--- server/database/index.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-
-module.exports = new Sequelize('Web_platforma_dlya_khudozhestvennykh_auktsionov', 'postgres', 'admin', {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-});
