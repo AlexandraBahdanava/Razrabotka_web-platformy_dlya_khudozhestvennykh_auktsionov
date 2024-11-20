b2dcd46e0d646e5ecf8237dee63f51b5e84418c8
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2024-11-20 19:43:30 +0300

    fix: fix the display of auctions on the main page

--- client/src/components/AllAuctions.jsx
+++ client/src/components/AllAuctions.jsx
@@ -2,11 +2,43 @@ import React, { useEffect, useState } from "react";
 import { Box, Typography, Paper, Grid } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { getAuctions } from "../api/auctionApi";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+// Компонент для вычисления и отображения текущей цены
+const CurrentPrice = ({ startingPrice, bids }) => {
+  const totalPrice = bids.reduce((sum, bid) => sum + bid.amount, startingPrice);
+  return <Typography variant="h6">{`$${totalPrice.toFixed(2)}`}</Typography>;
+};
+
+// Функция для вычисления оставшегося времени
+const calculateRemainingTime = (createdAt, duration) => {
+  const createdAtTime = new Date(createdAt).getTime();
+  const durationMs = duration * 24 * 60 * 60 * 1000; // Преобразуем дни в миллисекунды
+  const currentTime = new Date().getTime();
+  const remainingTime = createdAtTime + durationMs - currentTime;
+
+  if (remainingTime > 0) {
+    const days = Math.floor(remainingTime / (24 * 60 * 60 * 1000));
+    const hours = Math.floor(
+      (remainingTime % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+    );
+    const minutes = Math.floor(
+      (remainingTime % (60 * 60 * 1000)) / (60 * 1000)
+    );
+    const seconds = Math.floor((remainingTime % (60 * 1000)) / 1000);
+
+    return `${String(days).padStart(2, "0")}:${String(hours).padStart(
+      2,
+      "0"
+    )}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  } else {
+    return "Expired";
+  }
+};
 
 const AllAuctions = () => {
   const theme = useTheme();
-
+  const navigate = useNavigate();
   const [auctionData, setAuctionData] = useState([]);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
@@ -15,19 +47,15 @@ const AllAuctions = () => {
     const loadData = async () => {
       try {
         const response = await getAuctions();
-
         if (!response || response.status >= 300) {
           const errorMessage = response
             ? `Ошибка при загрузке данных. Код: ${response.status}`
             : "Сервис временно недоступен";
           displayError(errorMessage);
-          console.error(response || "Нет ответа от сервера");
           return;
         }
-
-        setAuctionData(response.data || []); // Защита на случай, если `data` будет `undefined`
+        setAuctionData(response.data || []);
       } catch (error) {
-        console.error("Ошибка при загрузке данных:", error);
         displayError("Не удалось загрузить данные. Попробуйте позже.");
       }
     };
@@ -40,73 +68,77 @@ const AllAuctions = () => {
     setError(true);
   };
 
-  const closeSnackbar = (event, reason) => {
-    if (reason === "clickaway") return;
-    setError(false);
-  };
-
   return (
     <>
       {auctionData.length > 0 ? (
-        <Grid
-          container
-          item
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="row"
-          flexWrap="wrap"
-          gap={2}
-        >
+        <Grid container spacing={2} justifyContent="flex-start">
           {auctionData.map((auction) => (
             <Grid
               key={auction.id}
               item
-              xs={1.6}
-              md={2}
-              lg={1.4}
-              margin="0 40px"
+              xs={12}
+              sm={6}
+              md={4}
+              lg={2.4}
+              onClick={() => navigate(`/auction/one/${auction.id}`)}
+              sx={{ cursor: "pointer" }}
             >
               <Paper
                 sx={{
                   borderRadius: "16px",
-                  p: 0,
                   display: "flex",
                   flexDirection: "column",
-                  flexWrap: "wrap",
+                  position: "relative",
+                  p: 2,
                   width: "260px",
-                  marginBottom: "220px",
+                  height: "350px",
                 }}
               >
-                <Link
-                  to={`/auction/one/${auction.id}`}
-                  style={{ textDecoration: "none", color: "#000000" }}
+                {/* Таймер обратного отсчета */}
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "8px",
+                    right: "8px",
+                    backgroundColor: theme.palette.success.main,
+                    color: "#fff",
+                    padding: "4px 8px",
+                    borderRadius: "8px",
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                  }}
                 >
-                  <img
-                    src={auction.photo}
-                    alt="Auction"
-                    style={{
-                      objectFit: "cover",
-                      width: "260px",
-                      height: "220px",
-                      borderRadius: "8px",
-                    }}
+                  <CountdownTimer
+                    createdAt={auction.createdAt}
+                    duration={auction.duration}
                   />
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      mt: 2,
-                      padding: "0 20px 10px 20px",
-                    }}
-                  >
-                    <Typography variant="h1">{auction.Artist?.name}</Typography>
-                    <Typography variant="h1">
-                      {`$${auction.starting_price}`}
-                    </Typography>
-                  </Box>
-                </Link>
+                </Box>
+                <img
+                  src={auction.photo}
+                  alt="Auction"
+                  style={{
+                    objectFit: "cover",
+                    width: "100%",
+                    height: "200px",
+                    borderRadius: "8px",
+                  }}
+                />
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    mt: 2,
+                  }}
+                >
+                  <Typography variant="body1" sx={{ fontWeight: 600 }}>
+                    {auction.Artist?.name}
+                  </Typography>
+                  <CurrentPrice
+                    startingPrice={auction.starting_price}
+                    bids={auction.bids || []}
+                  />
+                </Box>
               </Paper>
             </Grid>
           ))}
@@ -123,6 +155,23 @@ const AllAuctions = () => {
       )}
     </>
   );
+};
+
+// Таймер обратного отсчета
+const CountdownTimer = ({ createdAt, duration }) => {
+  const [remainingTime, setRemainingTime] = useState(
+    calculateRemainingTime(createdAt, duration)
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime(calculateRemainingTime(createdAt, duration));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [createdAt, duration]);
+
+  return <>{remainingTime}</>;
 };
 
 export default AllAuctions;
